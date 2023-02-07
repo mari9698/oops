1f816d4b94026fb563cff28e833748c0775c0da9
mari9698 <124653391+mari9698@users.noreply.github.com>
2023-02-07 10:45:21 +0530

    squareofnumbers

---
+++ sqareofnumbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class squareofnumbers
+public static void main(string[] args)
+{
+int n=5;
+for(i=1;i<=n;i++)
+{
+j=i*i;
+sum=sum+j;
+system.out.println("sum of square of terms:"+sum);
+}}
